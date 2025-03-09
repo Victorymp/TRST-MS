880921deaaac0586959dcca89d0d0b3b24cb18e3
Victory Mpokosa <mpokosa5@gmail.com>
2025-03-09 13:32:29 +0000

    Home Page

--- trst-ms/src/App.js
+++ trst-ms/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import TrostAgent from './components/TrostAgent';
+import TrostButton from './components/TrostButton';
+import './styles/globals.css';
+import './styles/Home.module.css';
+import './styles/landing.css';
+import './styles/main.css';
+
+function importAll(r) {
+  return r.keys().map(r);
+}
 
 function App() {
+  const carImages = importAll(require.context('./images/cars', false, /\.(png|jpe?g|svg)$/));
+  const [currentCar, setCurrentCar] = useState(0);
+  const nextCar = () => {
+    if (currentCar < carImages.length - 1) {
+      setCurrentCar(currentCar + 1);
+      console.log(currentCar);
+    } else {
+      setCurrentCar(0);
+    }
+  }
+  const backCar = () => {
+    if (currentCar > 0) {
+      setCurrentCar(currentCar - 1);
+      console.log(currentCar);
+    } else {
+      setCurrentCar(carImages.length - 1);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="dashboard">
+        <div className='item'>
+          <h1>TRST</h1>
+          <div className='item-content'>
+            <img src={carImages[currentCar]} alt='cars'/>
+            <div className='navigation-buttons'>
+              <button id='next' onClick={() => nextCar()}>Next</button>
+              <button id='back' onClick={() => backCar()}>Back</button>
+            </div>
+            <div className='item-text'>
+
+            </div>
+          </div>
+        </div>
+        <div className='button-group'>
+          <TrostAgent />
+          <TrostButton />
+        </div>
+      </div>
     </div>
   );
 }
